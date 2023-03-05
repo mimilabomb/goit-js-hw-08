f94013cac1d56d1041515ece4989eceaf3032bad
Bartosz Szymański <bartosz.szymanski@dsdik.local>
2023-03-05 19:49:53 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+// const bodycolor = document.querySelector('.body');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function timer() {
+  timerInterval = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(timerInterval);
+}
+
+startBtn.addEventListener('click', timer);
+stopBtn.addEventListener('click', stop);
